e00eb6555fc141a2173af6b9b7a0436c1a48bc5a
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2022-12-22 21:26:12 +0900

    Create ReadListenner

--- src/main/java/com/example/demo/listener/ReadListener.java
+++ src/main/java/com/example/demo/listener/ReadListener.java
@@ -1,4 +1,27 @@
 package com.example.demo.listener;
 
-public class ReadListener {
+import com.example.demo.domain.model.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ItemReadListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ReadListener implements ItemReadListener<Employee> {
+
+
+    @Override
+    public void beforeRead() {
+        // Do Nothing
+    }
+
+    @Override
+    public void afterRead(Employee item) {
+        log.debug("AfterRead:{}", item);
+    }
+
+    @Override
+    public void onReadError(Exception ex) {
+        log.error("ReadError: errorMessage", ex.getMessage(), ex);
+    }
 }
