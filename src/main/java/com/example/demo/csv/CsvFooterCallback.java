51e06cf5130373d8eefabcea263eafab7a8e70b3
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2022-12-22 21:43:30 +0900

    Create Callbck

--- src/main/java/com/example/demo/csv/CsvFooterCallback.java
+++ src/main/java/com/example/demo/csv/CsvFooterCallback.java
@@ -1,4 +1,20 @@
 package com.example.demo.csv;
 
-public class CsvFooterCallback {
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class CsvFooterCallback implements FlatFileFooterCallback {
+
+    @Value("#{StepExecution}")
+    private StepExecution stepExecution;
+
+    @Override
+    public void writeFooter(Writer writer) throws IOException {
+        writer.write("合計=" + stepExecution.getWriteCount() + "件");
+    }
 }
